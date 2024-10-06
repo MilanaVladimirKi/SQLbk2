8562e3985a8c0f8793cf5fa00037e4da768b75a8
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-10-06 05:08:48 +0500

    Valid from F to S

--- src/test/java/ru/topacademy/restSql/test/ApiSqlTest.java
+++ src/test/java/ru/topacademy/restSql/test/ApiSqlTest.java
@@ -1,4 +1,33 @@
 package ru.topacademy.restSql.test;
 
+import org.junit.jupiter.api.Test;
+import ru.topacademy.restSql.data.ApiHelper;
+import ru.topacademy.restSql.data.DataHelper;
+import ru.topacademy.restSql.data.SqlHelper;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ApiSqlTest {
+
+    @Test
+    public void validTransferFromFirstToSecond() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        ApiHelper.makeQueryToLogin(authInfo, 200);
+        var verificationCode = SqlHelper.getVerificationCode();
+        var verificationInfo = new DataHelper.VerificationInfo(authInfo.getLogin(), verificationCode.getCoda());
+        var tokenInfo = ApiHelper.sendQueryToVerify(verificationInfo, 200);
+        var cardsBalances = ApiHelper.sendQueryToGetCardsBalance(tokenInfo.getToken(), 200);
+        var firstCardBalance = cardsBalances.get(DataHelper.getFirstCardInfo().getId());
+        var secondCardBalance = cardsBalances.get(DataHelper.getSecondCardInfo().getId());
+        var amount = DataHelper.generateValidAmount(firstCardBalance);
+        var transferInfo = new ApiHelper.APITransferInfo(DataHelper.getFirstCardInfo().getNumber(),
+                DataHelper.getSecondCardInfo().getNumber(), amount);
+        ApiHelper.generateQueryToTransfer(tokenInfo.getToken(), transferInfo, 200);
+        cardsBalances = ApiHelper.sendQueryToGetCardsBalance(tokenInfo.getToken(), 200);
+        var actualFirstCardBalance = cardsBalances.get(DataHelper.getFirstCardInfo().getId());
+        var actualSecondCardBalance = cardsBalances.get(DataHelper.getSecondCardInfo().getId());
+        assertAll(( -> assertEquals(firstCardBalance - amount, actualFirstCardBalance),
+                () -> assertEquals(secondCardBalance + amount, actualSecondCardBalance));
+    }
 }
